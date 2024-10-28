3b123845ba8ec3689936b39d0ce6bc4557bec18c
sskmayu <103622498+sskmayu@users.noreply.github.com>
2024-10-28 14:46:26 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '9c86dbb4-c814-44af-b037-852ee8058848';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
